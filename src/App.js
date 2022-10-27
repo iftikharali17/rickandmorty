d5e3ed28d0220805247a5f10f356363922cfc0fc
Iftikhar Ali <61257031+iftikharali17@users.noreply.github.com>
2022-10-27 06:33:50 +0200

    Data fetching from Api done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'; //imported bootstrap css
+import 'bootstrap/dist/js/bootstrap'; //imported bootstrap js
 import './App.css';
 
+
+import React, {useEffect, useState} from "react";
+
 function App() {
+  const [fetchedData, setFetchedData] = useState([]) // api data state variable
+  const {info, results} = fetchedData; // destructuring api data
+
+  const api = `https://rickandmortyapi.com/api/character/`; //api url from documentation of api
+
+  useEffect( () => {
+    (async function(){
+      let data = await fetch(api).then((res) => res.json());
+      setFetchedData(data)
+    })();
+  },[api]);
+
+  console.log(results) // data fetching correct
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="my-4">Rick & Morty Character Search Engine</h1>
+
     </div>
   );
 }
